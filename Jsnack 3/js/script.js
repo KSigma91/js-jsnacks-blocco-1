56d3e52fb079e27a39d3fe6794a2047cc73fdc7c
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-11 15:37:00 +0200

    Jsnack 3

--- Jsnack 3/js/script.js
+++ Jsnack 3/js/script.js
@@ -1,12 +1,36 @@
 // do una lista di parole da mettere in ordine
-const list1 = ['parola1 arr1', 'parola2 arr1', 'parola3 arr1', 'parola4 arr1', 'parola5 arr1'];
+const list1 = ["parola1-1", "parola2-1", "parola3-1", "parola4-1", "parola5-1"];
 
-const list2 = ['parola1 arr2', 'parola2 arr2'];
+const list2 = ["parola1-2", "parola2-2"];
 
+// seleziono la prima lista
+const myAppend = document.querySelector(".my_list");
 
-for (let i = 0; i < 5; i++) {
+// prima lista
+for (let i = 0; i < list1.leght; i++) {
 
-    const listNum = document.createElement('li');
-    
-    listNum.append(list1[i]);
+    let myCont = list1[i];
+
+    let myLi = document.createElement("li");
+
+    myLi.append(myCont);
+
+    myAppend.append(myLi);
+
+    // seleziono la seconda lista
+    let myAppend2 = document.createElement("ul");
+
+    // seconda lista
+    for (let j = 0; j < list2.length; j++) {
+
+        let myCont2 = list2[j];
+
+        let myLi2 = document.createElement("li");
+
+        myLi2.append(myCont2);
+
+        myAppend2.append(myLi2);
+
+        myLi.append(myAppend2);
+    }
 }
